629e7ddda6c816b8b3e9f807aeef288ee8fb5771
YiZhang <hyacinthzy1@gmail.com>
2018-04-27 12:07:04 +1000

    Add POST /users route and token method

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,10 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;   // set up the mongoose promise
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost: 27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {
   mongoose
 };
+
+process.env.NODE_ENV
